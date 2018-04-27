c6eb983c16638150357f92922ddb23de2e00386c
David Andersson <David Andersson>
2018-04-27 10:55:14 +0200

    Lifting state example

--- WebApplication6/ClientApp/components/Forest.tsx
+++ WebApplication6/ClientApp/components/Forest.tsx
@@ -6,10 +6,11 @@ interface IForestProps {
 }
 interface IForestState {
     squirrel: boolean;
+    isHidden: boolean;
 }
 
 export class Forest extends React.Component<RouteComponentProps<IForestProps>, IForestState> {
-    state = {squirrel: false}
+    state = {squirrel: false, isHidden: false}
     /*constructor(props) {
         super(props);
         this.state = {
@@ -17,17 +18,32 @@ export class Forest extends React.Component<RouteComponentProps<IForestProps>, I
         }
     }*/
     public render() {
-        let info;
+        let info, squirrelStatus;
         if (this.state.squirrel)
             info = <span>A squirrel is here</span>;
         else
             info = <span>No squirrel in sight</span>;
+        if (this.state.isHidden)
+            squirrelStatus = <div>Squirrel has hidden</div>;
+        else
+            squirrelStatus = <div>Squirrel is not hidden</div>;
+
+        let stuff = ['blueberries', 'stones', 'grass']
+            .map((x, index) => <ForestStuff key={x+index}>{x}</ForestStuff>);
         return <div>
             Welcome to the forest! <br />
             <button onClick={() => this.callSquirrel()}>Call the squirrel!</button>
             <br />
             {info}
+            <br />
+            <br />
+            <ul>{stuff}</ul>
+            <Squirrel squirrelHide={() => this.squirrelHide()} />
+            {squirrelStatus}
         </div>;
+    }
+    squirrelHide() {
+        this.setState({ isHidden: true });
     }
 
     callSquirrel() {
@@ -35,4 +51,26 @@ export class Forest extends React.Component<RouteComponentProps<IForestProps>, I
             squirrel: true
         });
     }
+}
+interface ISquirrelProps {
+    squirrelHide: any;
+}
+class Squirrel extends React.Component<ISquirrelProps, {}> {
+    render() {
+        return <button onClick={() => this.clickedHideButton()}>Hide</button>;
+    }
+    clickedHideButton() {
+        // informera Forest-komponenten om vad som hänt
+        this.props.squirrelHide();
+    }
+}
+
+interface IForestStuffProps {
+    //stuff: string;
+    children?: any;
+}
+function ForestStuff(props : IForestStuffProps) {
+    //return <li>{props}</li>;
+    //let x = { value: 5 };
+    return <li>{props.children}</li>;
 }
